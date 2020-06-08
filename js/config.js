44d85688b5c72fb3582066c45bcbb2eb6663f349
harkirat22 <harkiratbhardwaj@gmail.com>
2020-06-08 16:57:40 +1000

    New Changes

--- js/config.js
+++ js/config.js
@@ -1,7 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_12HYouKzJ',
+        userPoolId: 'us-east-1_bR93arB1q',
         region: 'us-east-1',
-        clientId: '59pepohdkvu7g8sufcma46325n'
+        clientId: '4jdpq3c6l6hgbt4unf23ltsg6c'
     },
+
 };
